5dada26a4c48d3ae7ee6ff7c44a8b246891e93ba
Konrad Synowiec <64809815+Kkoonnrr@users.noreply.github.com>
2024-07-17 15:43:08 +0800

    Divide event functions to different file - final version

--- EEG_PL/get_events.py
+++ EEG_PL/get_events.py
@@ -1,22 +1,33 @@
 import pandas as pd
 from datetime import datetime, timedelta
 from pathlib import Path
+import mne
+
+eg_channels = ['Cz', 'Fz', 'Fp1', 'F7', 'F3', 'FC1', 'C3', 'FC5', 'FT9', 'T7', 'CP5', 'CP1', 'P3', 'P7', 'PO9',
+               'O1', 'Pz', 'Oz', 'O2', 'PO10', 'P8', 'P4', 'CP2', 'CP6', 'T8', 'FT10', 'FC6', 'C4', 'FC2', 'F4',
+               'F8', 'Fp2']
+
+
+def to_date(date: str):
+    return datetime.strptime(date, '%H:%M:%S.%f')
 
 
 class Events:
-    def __init__(self):
+    def __init__(self, dir_name: str, eeg_edf: str, eeg_csv):
+        self.dir_name = dir_name
+        self.eeg_csv = eeg_csv
+        self.eeg_edf = eeg_edf
         self.directories = None
         self.experiment_data = {}
 
-    def create_trial_files(self, path: str):
-        base_path = Path('../Data/EEG_PL/11.07') / path
+    def create_trial_files(self):
+        base_path = Path('../Data/EEG_PL/11.07') / self.dir_name
         directories = [d for d in base_path.iterdir() if d.is_dir()]
         self.directories = directories
         for directory in directories:
             gaze_files = [f for f in directory.iterdir() if 'gaze' in f.name]
             if not gaze_files:
                 continue  # Skip if no gaze files found
-
             gaze_file = gaze_files[0]  # Assuming only one gaze file per directory
             self.process_gaze_file(gaze_file, directory)
 
@@ -26,10 +37,8 @@ class Events:
         except Exception as e:
             print(f"Error reading {gaze_file}: {e}")
             return
-
         click_data = gaze_data[gaze_data['KBS'] == 1]
         events = self.extract_events(click_data)
-
         if not events.empty:
             output_file = directory / "Good_Experiments.csv"
             events.to_csv(output_file, index=False)
@@ -41,14 +50,11 @@ class Events:
     def extract_events(self, click_data: pd.DataFrame) -> pd.DataFrame:
         columns = ["Time Of Experiment", "Duration", "TimeStampStart", "TimeStampEnd"]
         events = pd.DataFrame(columns=columns)
-
         for index in range(len(click_data) - 1):
             start_time = click_data.iloc[index, 1]
             duration = click_data.iloc[index + 1, 1] - start_time
-
             if not (3.9 <= duration <= 4.1):
                 continue
-
             try:
                 start_timestamp = datetime.strptime(click_data.columns[1][16:-1], "%H:%M:%S.%f")
             except ValueError as e:
@@ -64,9 +70,9 @@ class Events:
                 "TimeStampStart": delta_start.strftime("%H:%M:%S.%f")[:-3],
                 "TimeStampEnd": delta_end.strftime("%H:%M:%S.%f")[:-3]
             })
-
         return events
 
+    # TODO
     def load_experiment_data(self):
         experiment_data = {}
         for directory in self.directories:
@@ -76,6 +82,35 @@ class Events:
 
     def create_start_stop_df_for_events(self, start_time: str):
         if not self.experiment_data:
-            self.experiment_data = self.load_experiment_data()
+            self.experiment_data = self.load_experiment_data()  # not working
+        event_col = ['directory',
+                     'start_of_the_event(EEG data sec)',
+                     'end_of_the_event(EEG data sec)',
+                     'duration_of_the_event']
+        event_duration = pd.DataFrame(columns=event_col)
+        for index, data in enumerate(self.experiment_data.items()):
+            name, ex_data = data
+            event_duration.loc[index] = [name.name,
+                                         (to_date(ex_data.iloc[0]["TimeStampStart"]) -
+                                          to_date(start_time)).seconds,
+                                         (to_date(ex_data.iloc[-1]["TimeStampEnd"]) -
+                                          to_date(start_time)).seconds,
+                                         (to_date(ex_data.iloc[-1]["TimeStampEnd"]) -
+                                          to_date(ex_data.iloc[0]["TimeStampStart"])).seconds]
+        event_duration.to_csv(f"../Data/EEG_PL/11.07/{self.dir_name}/Event_time_count_by_EGG_data.csv")
+        return event_duration
 
+    def create_eeg_events_files(self):
+        raw_csv = pd.read_csv(self.eeg_csv, nrows=1)
+        start_time = datetime.fromtimestamp(float(raw_csv.columns[1].split(":")[1])).strftime('%H:%M:%S.%f')[:-3]
+        raw = mne.io.read_raw_edf(self.eeg_edf, preload=True)
+        raw.pick(eg_channels)
+        events_duration = self.create_start_stop_df_for_events(start_time)
+
+        for event_duration in events_duration.iterrows():
+            directory_name = event_duration[1]['directory']
+            start_time = event_duration[1]['start_of_the_event(EEG data sec)']
+            end_time = event_duration[1]['end_of_the_event(EEG data sec)']
+            raw_segment = raw.copy().crop(tmin=start_time, tmax=end_time)
+            raw_segment.save(f'../Data/EEG_PL/11.07/EEG/EEG_Event_data/{directory_name}_raw.fif', overwrite=True)
 
--- EEG_PL/main.py
+++ EEG_PL/main.py
@@ -12,43 +12,43 @@ from scipy.integrate import simps
 from mne.preprocessing import ICA
 from mne.time_frequency import tfr_morlet
 from get_events import Events
+
 mpl.use("MacOSX")
 
 if __name__ == "__main__":
-    events = Events()
-    # events.create_trial_files('small')
+    events = Events('small',
+                    '../Data/EEG_PL/11.07/EEG/see all_EPOCFLEX_229567_2024.07.11T14.40.32+08.00.edf',
+                    '../Data/EEG_PL/11.07/EEG/see all_EPOCFLEX_229567_2024.07.11T14.40.32+08.00.md.csv')
+    # events.create_trial_files()
+    # events.create_eeg_events_files()
+    names = ['see_tree']
+    # events_duration = events.create_start_stop_df_for_events()
+    for name in names:
 
-    raw_csv = pd.read_csv('../Data/EEG_PL/11.07/EEG/see all_EPOCFLEX_229567_2024.07.11T14.40.32+08.00.md.csv',
-                          nrows=1)
-    start_time = datetime.fromtimestamp(float(raw_csv.columns[1].split(":")[1])).strftime('%H:%M:%S.%f')
-    events.create_start_stop_df_for_events(start_time)
-    raw = mne.io.read_raw_edf('../Data/EEG_PL/11.07/EEG/see all_EPOCFLEX_229567_2024.07.11T14.40.32+08.00.edf'
-                              , preload=True)
-    # get_events('small')
+        raw = mne.io.read_raw_fif(f'../Data/EEG_PL/11.07/EEG/EEG_Event_data/{name}_raw.fif', preload=True)
 
-    eg_channels = ['Cz', 'Fz', 'Fp1', 'F7', 'F3', 'FC1', 'C3', 'FC5', 'FT9', 'T7', 'CP5', 'CP1', 'P3', 'P7', 'PO9',
-                   'O1', 'Pz', 'Oz', 'O2', 'PO10', 'P8', 'P4', 'CP2', 'CP6', 'T8', 'FT10', 'FC6', 'C4', 'FC2', 'F4',
-                   'F8', 'Fp2']
-    # raw = mne.io.read_raw_edf('../Data/EEG_PL/07.02_EPOCFLEX_228045_2024.07.02T13.54.25+08.00.edf')
-    raw.pick(eg_channels)
 
-    raw.plot()
-    print(raw)
-    print(raw.info)
-    #
-    # print(raw.info['ch_names'])  # 打印所有通道名称
-    # print(raw.get_channel_types())  # 获取并打印所有通道的类型
 
-    montage = mne.channels.make_standard_montage('standard_1020')
-    raw.set_montage(montage, on_missing='ignore')
+        # raw.plot()
+        # print(raw)
+        # print(raw.info)
+        #
+        # print(raw.info['ch_names'])  # 打印所有通道名称
+        # print(raw.get_channel_types())  # 获取并打印所有通道的类型
 
-    raw.filter(0.1, 30, fir_design='firwin')
+        montage = mne.channels.make_standard_montage('standard_1020')
+        raw.set_montage(montage, on_missing='ignore')
 
-    # raw.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
-    # raw.plot(duration=5, n_channels=30)
-    #
-    # ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
-    # ica.fit(raw)
-    # ica.exclude = [1, 2]  # details on how we picked these are omitted here
-    # ica.plot_properties(raw, picks=ica.exclude)
-    ...
+        raw.filter(0.1, 40, fir_design='firwin')
+
+        raw.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
+        raw.plot(duration=5, n_channels=30)
+        #
+        ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
+        ica.fit(raw)
+        ica.plot_components()
+        ica.exclude = [0]
+        raw_ica = ica.apply(raw)
+        raw_ica.plot()
+        ...
+        # ica.plot_properties(raw, picks=ica.exclude)
